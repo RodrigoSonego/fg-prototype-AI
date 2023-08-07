9468f4a3a435dcd5600bba672da00145aa472d86
Rodrigo Sonego <rodrigoasonego@gmail.com>
2023-08-07 10:18:16 -0300

    feature: movement, walking forward and backward

---
+++ Assets/Fighter/Fighter.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Fighter : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float walkBackSpeed;
+	[SerializeField] private Fighter opponent;
+
+	private PlayerInput input;
+	private Rigidbody2D rb;
+
+	private float distanceToOpponent;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+
+		input = new PlayerInput();
+		input.Fighter.Walk.Enable();
+
+		if(opponent ==  null) { return; }
+		print(opponent.name);
+	}
+
+	private void FixedUpdate()
+	{
+		float inputDirection = input.Fighter.Walk.ReadValue<float>();
+
+		if(opponent == null ) { return; }
+		distanceToOpponent = opponent.transform.position.x - rb.position.x;
+
+		if(WillWalkBack(inputDirection))
+		{
+			WalkBack(inputDirection);
+		}
+		else
+		{
+			Walk(inputDirection);
+		}
+	}
+
+	private bool WillWalkBack(float inputDirection)
+	{
+		return distanceToOpponent * inputDirection < 0;
+	}
+
+	private void Walk(float direction)
+	{
+		float newX = transform.position.x + (moveSpeed * direction * Time.deltaTime);
+		rb.MovePosition(new (newX, rb.position.y));
+	}
+
+	private void WalkBack(float direction)
+	{
+		float newX = transform.position.x + (walkBackSpeed * direction * Time.deltaTime);
+		rb.MovePosition(new(newX, rb.position.y));
+	}
+}
